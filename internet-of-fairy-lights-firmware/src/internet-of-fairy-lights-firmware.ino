9876a50dc24584292b1cec36c38384eb00ae7d53
Evan Simkowitz <e.simkowitz@gmail.com>
2019-01-27 16:18:50 -0600

    added inter-device communication, platform-specific features

--- internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
+++ internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
@@ -8,9 +8,14 @@
 #define fairy_light_pin_1 A0
 #define fairy_light_pin_2 A2
 
-int change_mode(String new_mode);
+int change_mode(String new_mode, bool publish);
+
+#if (PLATFORM_ID == PLATFORM_ARGON)
+int change_mode_function_handler(String new_mode);
+#endif
 
 enum Mode {
+  off,
   alternate_blink,
   steady
 } blink_mode;
@@ -24,20 +29,35 @@ unsigned long steady_delta_time = 2;
 bool steady_state = 0;
 
 
+void change_mode_subscribe_handler(const char *event, const char *data)
+{
+  String new_mode = String(data);
+  change_mode(new_mode, 0);
+}
+
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
   pinMode(fairy_light_pin_1, OUTPUT);
   pinMode(fairy_light_pin_2, OUTPUT);
 
-  Particle.function("change_mode", change_mode);
+  #if (PLATFORM_ID == PLATFORM_ARGON)
+  Particle.function("change_mode", change_mode_function_handler);
+  #endif
 
-  blink_mode = alternate_blink;
+  Mesh.subscribe("new_mode", change_mode_subscribe_handler);
+
+  blink_mode = off;
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   switch (blink_mode) {
+    case off:
+    {
+      analogWrite(fairy_light_pin_1, 0);
+      analogWrite(fairy_light_pin_2, 0);
+      break;
+    }
     case alternate_blink:
     {
       if (millis() - alternate_blink_last_time > alternate_blink_delta_time) {
@@ -71,12 +91,23 @@ void loop() {
   }  
 }
 
-int change_mode(String new_mode) {
+#if (PLATFORM_ID == PLATFORM_ARGON)
+int change_mode_function_handler(String new_mode) {
+  return change_mode(new_mode, 1);
+}
+#endif
+
+int change_mode(String new_mode, bool publish) {
   new_mode = new_mode.toLowerCase();
-  if (new_mode == "alternate_blink") {
+  if (new_mode == "off") {
+    blink_mode = off;
+  } else if (new_mode == "alternate_blink") {
     blink_mode = alternate_blink;
   } else if (new_mode == "steady") {
     blink_mode = steady;
   } else return -1;
+  if (publish == 1) {
+    Mesh.publish("new_mode", new_mode);
+  }
   return 1;
 }

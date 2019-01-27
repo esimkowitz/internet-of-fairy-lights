dca81a1393c44781ac2f6dc7e8ca2314a47f7aba
Evan Simkowitz <e.simkowitz@gmail.com>
2019-01-27 17:40:21 -0600

    simplifying the preprocessor work

--- internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
+++ internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
@@ -8,19 +8,27 @@
 #define fairy_light_pin_1 A0
 #define fairy_light_pin_2 A2
 
-int change_mode(String new_mode, bool publish);
+// Define the functions to handle the change_mode functionality
+int change_mode(String new_mode);
 
-#if (PLATFORM_ID == PLATFORM_ARGON)
-int change_mode_function_handler(String new_mode);
+// Only the gateway (the Argon device) needs the cloud-facing change_mode function, the others just subscribe.
+// This function is the handler for the subscribed event.
+#if (PLATFORM_ID == PLATFORM_XENON)
+void change_mode_subscribe_handler(const char *event, const char *data)
+{
+  String new_mode = String(data);
+  change_mode(new_mode);
+}
 #endif
 
-enum Mode {
+enum Mode
+{
   off,
   alternate_blink,
   steady
 } blink_mode;
 
-
+// Set up the timers for the various blink modes
 void off_handler()
 {
   analogWrite(fairy_light_pin_2, 0);
@@ -34,10 +42,13 @@ bool alternate_blink_state = false;
 
 void alternate_blink_handler()
 {
-  if (alternate_blink_state == false) {
+  if (alternate_blink_state == false)
+  {
     analogWrite(fairy_light_pin_1, 0);
     analogWrite(fairy_light_pin_2, 230);
-  } else {
+  }
+  else
+  {
     analogWrite(fairy_light_pin_2, 0);
     analogWrite(fairy_light_pin_1, 230);
   }
@@ -51,10 +62,13 @@ bool steady_state = false;
 
 void steady_handler()
 {
-  if (steady_state == false) {
+  if (steady_state == false)
+  {
     analogWrite(fairy_light_pin_1, 0);
     analogWrite(fairy_light_pin_2, 230);
-  } else {
+  }
+  else
+  {
     analogWrite(fairy_light_pin_2, 0);
     analogWrite(fairy_light_pin_1, 230);
   }
@@ -63,83 +77,87 @@ void steady_handler()
 
 Timer steady_timer(steady_delta_time, steady_handler);
 
-
-void change_mode_subscribe_handler(const char *event, const char *data)
-{
-  String new_mode = String(data);
-  change_mode(new_mode, false);
-}
-
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   pinMode(fairy_light_pin_1, OUTPUT);
   pinMode(fairy_light_pin_2, OUTPUT);
 
   analogWrite(fairy_light_pin_1, 0);
   analogWrite(fairy_light_pin_2, 0);
 
-  #if (PLATFORM_ID == PLATFORM_ARGON)
-  Particle.function("change_mode", change_mode_function_handler);
-  #endif
-
+#if (PLATFORM_ID == PLATFORM_ARGON)
+  Particle.function("change_mode", change_mode);
+#elif (PLATFORM_ID == PLATFORM_XENON)
   Mesh.subscribe("new_mode", change_mode_subscribe_handler);
+#endif
 
   blink_mode = off;
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void loop()
+{
   // do nothing, everything is handled by timers
 }
 
-#if (PLATFORM_ID == PLATFORM_ARGON)
-int change_mode_function_handler(String new_mode) {
-  return change_mode(new_mode, true);
-}
-#endif
-
-void change_timer(Mode old_mode, Mode new_mode) {
-  switch (old_mode) {
-    case off:
-      // no need to stop off_timer, it stops automatically
-      break;
-    case steady:
-      steady_timer.stop();
-      break;
-    case alternate_blink:
-      alternate_blink_timer.stop();
-    default:
-      break;
+void change_timer(Mode old_mode, Mode new_mode)
+{
+  switch (old_mode)
+  {
+  case off:
+    // no need to stop off_timer, it stops automatically
+    break;
+  case steady:
+    steady_timer.stop();
+    break;
+  case alternate_blink:
+    alternate_blink_timer.stop();
+  default:
+    break;
   }
-  switch (new_mode) {
-    case off:
-      off_timer.start();
-      break;
-    case steady:
-      steady_timer.start();
-      break;
-    case alternate_blink:
-      alternate_blink_timer.start();
-    default:
-      break;
+  switch (new_mode)
+  {
+  case off:
+    off_timer.start();
+    break;
+  case steady:
+    steady_timer.start();
+    break;
+  case alternate_blink:
+    alternate_blink_timer.start();
+  default:
+    break;
   }
   return;
 }
 
-int change_mode(String new_mode, bool publish) {
+int change_mode(String new_mode)
+{
   new_mode = new_mode.toLowerCase();
   Mode old_mode = blink_mode;
-  if (new_mode == "off") {
+  if (new_mode == "off")
+  {
     blink_mode = off;
-  } else if (new_mode == "alternate_blink") {
-    blink_mode = alternate_blink;
-  } else if (new_mode == "steady") {
-    blink_mode = steady;
-  } else return -1;
-  if (publish == true) {
-    Mesh.publish("new_mode", new_mode);
   }
-  if (old_mode != blink_mode) {
+  else if (new_mode == "alternate_blink")
+  {
+    blink_mode = alternate_blink;
+  }
+  else if (new_mode == "steady")
+  {
+    blink_mode = steady;
+  }
+  else
+    return -1;
+
+// The gateway (the Argon device) publishes a Mesh event that the other devices listen to.
+#if (PLATFORM_ID == PLATFORM_ARGON)
+  Mesh.publish("new_mode", new_mode);
+#endif
+
+  if (old_mode != blink_mode)
+  {
     change_timer(old_mode, blink_mode);
   }
   return 1;

dea57aebe20deae5e62778577395f31c06c919e0
Evan Simkowitz <e.simkowitz@gmail.com>
2019-01-27 18:12:03 -0600

    adding ability to adjust brightness remotely

--- internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
+++ internet-of-fairy-lights-firmware/src/internet-of-fairy-lights-firmware.ino
@@ -8,18 +8,9 @@
 #define fairy_light_pin_1 A0
 #define fairy_light_pin_2 A2
 
-// Define the functions to handle the change_mode functionality
-int change_mode(String new_mode);
+#define fairy_light_high 230
 
-// Only the gateway (the Argon device) needs the cloud-facing change_mode function, the others just subscribe.
-// This function is the handler for the subscribed event.
-#if (PLATFORM_ID == PLATFORM_XENON)
-void change_mode_subscribe_handler(const char *event, const char *data)
-{
-  String new_mode = String(data);
-  change_mode(new_mode);
-}
-#endif
+float brightness;
 
 enum Mode
 {
@@ -27,6 +18,33 @@ enum Mode
   alternate_blink,
   steady
 } blink_mode;
+
+// Define the functions to handle the change_mode functionality
+int change_mode(String new_mode);
+
+// Only the gateway (the Argon device) needs the cloud-facing change_mode function, the others just subscribe.
+// This function is the handler for the subscribed event.
+#if (PLATFORM_ID == PLATFORM_XENON)
+void new_mode_subscribe_handler(const char *event, const char *data)
+{
+  String new_mode = String(data);
+  change_mode(new_mode);
+}
+#endif
+
+// Define the functions to handle the change_brightness functionality
+int change_brightness(String new_brightness);
+
+// Only the gateway (the Argon device) needs the cloud-facing change_brightness function, the others just subscribe.
+// This function is the handler for the subscribed event.
+#if (PLATFORM_ID == PLATFORM_XENON)
+void new_brightness_subscribe_handler(const char *event, const char *data)
+{
+  String new_brightness = String(data);
+  change_brightness(new_brightness);
+}
+#endif
+
 
 // Set up the timers for the various blink modes
 void off_handler()
@@ -45,12 +63,12 @@ void alternate_blink_handler()
   if (alternate_blink_state == false)
   {
     analogWrite(fairy_light_pin_1, 0);
-    analogWrite(fairy_light_pin_2, 230);
+    analogWrite(fairy_light_pin_2, fairy_light_high * brightness);
   }
   else
   {
     analogWrite(fairy_light_pin_2, 0);
-    analogWrite(fairy_light_pin_1, 230);
+    analogWrite(fairy_light_pin_1, fairy_light_high * brightness);
   }
   alternate_blink_state = !alternate_blink_state;
 }
@@ -65,12 +83,12 @@ void steady_handler()
   if (steady_state == false)
   {
     analogWrite(fairy_light_pin_1, 0);
-    analogWrite(fairy_light_pin_2, 230);
+    analogWrite(fairy_light_pin_2, fairy_light_high * brightness);
   }
   else
   {
     analogWrite(fairy_light_pin_2, 0);
-    analogWrite(fairy_light_pin_1, 230);
+    analogWrite(fairy_light_pin_1, fairy_light_high * brightness);
   }
   steady_state = !steady_state;
 }
@@ -87,12 +105,15 @@ void setup()
   analogWrite(fairy_light_pin_2, 0);
 
 #if (PLATFORM_ID == PLATFORM_ARGON)
-  Particle.function("change_mode", change_mode);
+  Particle.function("mode", change_mode);
+  Particle.function("brightness", change_brightness);
 #elif (PLATFORM_ID == PLATFORM_XENON)
-  Mesh.subscribe("new_mode", change_mode_subscribe_handler);
+  Mesh.subscribe("new_mode", new_mode_subscribe_handler);
+  Mesh.subscribe("new_brightness", new_brightness_subscribe_handler);
 #endif
 
   blink_mode = off;
+  brightness = 1.0;
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -160,5 +181,21 @@ int change_mode(String new_mode)
   {
     change_timer(old_mode, blink_mode);
   }
+  return 1;
+}
+
+int change_brightness(String new_brightness) {
+  int new_brightness_int = new_brightness.toInt();
+  if (new_brightness_int < 0 || new_brightness_int > 10) {
+    return -1;
+  }
+  
+  brightness = new_brightness_int / 10.0;
+
+  // The gateway (the Argon device) publishes a Mesh event that the other devices listen to.
+#if (PLATFORM_ID == PLATFORM_ARGON)
+  Mesh.publish("new_brightness", new_brightness);
+#endif
+
   return 1;
 }
